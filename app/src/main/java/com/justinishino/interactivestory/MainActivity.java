f1f1258ed404d8a2db840b581c73b6b285518526
Justin Ishino <jishino89@gmail.com>
2019-03-05 13:02:57 -0700

    Get input form EditText

--- app/src/main/java/com/justinishino/interactivestory/MainActivity.java
+++ app/src/main/java/com/justinishino/interactivestory/MainActivity.java
@@ -1,5 +1,6 @@
 package com.justinishino.interactivestory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,8 +25,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = nameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+
+                startStory();
             }
         });
+    }
+
+    private void startStory() {
+        Intent intent = new Intent(this, StoryActivity.class);
+        startActivity(intent);
     }
 }
