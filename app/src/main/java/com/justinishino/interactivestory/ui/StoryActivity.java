d83ffe9b0c7eefa34768a2c9129888c0cdaecd1b
Justin Ishino <jishino89@gmail.com>
2019-03-18 13:02:32 -0600

    Adding Page content.

--- app/src/main/java/com/justinishino/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/justinishino/interactivestory/ui/StoryActivity.java
@@ -18,6 +18,7 @@ public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
 
+    private String name;
     private Story story;
     private ImageView storyImageView;
     private TextView storyTextView;
@@ -35,7 +36,7 @@ public class StoryActivity extends AppCompatActivity {
         choice2Button = findViewById(R.id.choice2Button);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        name = intent.getStringExtra(getString(R.string.key_name));
         if (name == null || name.isEmpty()) {
             name = "Friend";
         }
@@ -51,6 +52,12 @@ public class StoryActivity extends AppCompatActivity {
         Drawable image = ContextCompat.getDrawable(this, page.getImageId());
         storyImageView.setImageDrawable(image);
 
+        String pageText = getString(page.getTextId());
+        // Add name if place holder is used
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
 
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice2Button.setText(page.getChoice2().getTextId());
     }
 }
